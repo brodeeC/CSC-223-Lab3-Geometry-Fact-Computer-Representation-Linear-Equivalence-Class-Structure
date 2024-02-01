0a379bdb5d8b37a06a890de75f473e19f594a207
brodeeC <brodeeclontz@gmail.com>
2024-02-01 16:06:59 -0500

    Constructor for LinkedList

--- src/utilities/LinkedList/LinkedList.java
+++ src/utilities/LinkedList/LinkedList.java
@@ -1,5 +1,18 @@
 package utilities.LinkedList;
 
-public class LinkedList {
-
+public class LinkedList <T>{
+	
+	protected int _size;
+	protected DNode _head;
+	protected DNode _tail;
+	
+	protected class DNode {
+		protected T _item;
+		protected DNode _next;
+		protected DNode _prev;
+		
+		public DNode() {this(null, null, null); }
+		
+		public DNode (DNode p, T i, DNode n) {_prev = p; _item = i; _next = n; }
+	}
 }
